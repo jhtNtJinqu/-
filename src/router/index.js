c15f0859246b0c513fcfaf697ea1d3cdff5b9a63
e <15262870437@163.com>
2017-09-05 17:02:50 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/login'
-
+import Register from '@/components/register'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,10 @@ export default new Router({
       path: '/',
       name: 'Login',
       component: Login
+    },{
+      path: '/register',
+      name: 'Register',
+      component: Register
     }
   ]
 })
