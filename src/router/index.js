0c7224e28613d76cabc5a26970af6b7e2e8749c4
e <15262870437@163.com>
2017-09-06 09:49:37 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/login'
-import Register from '@/views/register'
+import Register from '@/components/register'
 Vue.use(Router)
 
 export default new Router({
